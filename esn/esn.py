67b768cd9529708e6c8fbb60522679524e296e4f
dtk <dtk@gmx.de>
2017-01-13 21:11:30 +0100

    Draft a test run

--- esn/esn.py
+++ esn/esn.py
@@ -87,3 +87,29 @@ class ESN(object):
                 # TODO Add `W_fb` here
             )
         return np.dot(self.W_out, np.vstack((1, input_date, self.x)))
+
+
+if __name__ == '__main__':
+    from sys import argv
+    data = np.loadtxt(argv[1])
+
+    esn = ESN(
+        in_size=1,
+        reservoir_size=1000,
+        out_size=1,
+        spectral_radius=1.25,
+        leaking_rate=0.3,
+        washout=100,
+        smoothing_factor=0.0001
+    )
+
+    esn.fit(data[:1000], data[None, 1:1001])
+
+    for value in range(1000, 2000):
+        input_date = data[value]
+        y_pred = esn.predict(input_date)
+        print('{} -> {} (target: {}) | Δ: {}'.format(
+            input_date,
+            y_pred,
+            data[value+1],
+            data[value+1] - y_pred))
