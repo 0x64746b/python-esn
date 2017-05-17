a6608f3df98801643ad047bae796b37cbb381a40
dtk <dtk@gmx.de>
2017-05-17 18:05:54 +0200

    Parallelize hyperopt runs

--- examples/sine/simple.py
+++ examples/sine/simple.py
@@ -11,9 +11,9 @@ from __future__ import (
 )
 
 import logging
-import pickle
 
 import hyperopt
+import hyperopt.mongoexp
 import numpy as np
 import pandas as pd
 import scipy
@@ -124,25 +124,6 @@ class Example(object):
         return np.array(predicted_outputs)
 
     def optimize(self, exp_key):
-        def objective(hyper_parameters):
-            # re-seed for repeatable results
-            np.random.seed(48)
-
-            try:
-                predicted_outputs = self._train(*hyper_parameters)
-            except scipy.sparse.linalg.ArpackNoConvergence:
-                return {'status': hyperopt.STATUS_FAIL}
-            else:
-                try:
-                    rmse = np.sqrt(mean_squared_error(
-                        self.test_outputs,
-                        predicted_outputs
-                    ))
-                except ValueError:
-                    return {'status': hyperopt.STATUS_FAIL}
-                else:
-                    return {'status': hyperopt.STATUS_OK, 'loss': rmse}
-
         search_space = (
             hyperopt.hp.quniform('spectral_radius', 0, 1.5, 0.01),
             hyperopt.hp.quniform('leaking_rate', 0, 1, 0.01),
@@ -153,20 +134,36 @@ class Example(object):
             hyperopt.hp.quniform('input_noise', 1e-10, 1e-2, 1e-10),
         )
 
-        trials = hyperopt.Trials(exp_key=exp_key)
+        trials = hyperopt.mongoexp.MongoTrials(
+            'mongo://localhost:27017/python_esn_trials/jobs',
+            exp_key=exp_key,
+        )
 
         best = hyperopt.fmin(
-            objective,
+            self._objective,
             space=search_space,
             algo=hyperopt.tpe.suggest,
             max_evals=150,
             trials=trials,
         )
 
-        with open('{}_trials.pickle'.format(exp_key), 'wb') as trials_file:
-            pickle.dump(trials, trials_file)
-
-        with open('{}_best.pickle'.format(exp_key), 'wb') as result_file:
-            pickle.dump(best, result_file)
-
         logger.info('Best parameter combination: %s', best)
+
+    def _objective(self, hyper_parameters):
+        # re-seed for repeatable results
+        np.random.seed(48)
+
+        try:
+            predicted_outputs = self._train(*hyper_parameters)
+        except scipy.sparse.linalg.ArpackNoConvergence:
+            return {'status': hyperopt.STATUS_FAIL}
+        else:
+            try:
+                rmse = np.sqrt(mean_squared_error(
+                    self.test_outputs,
+                    predicted_outputs
+                ))
+            except ValueError:
+                return {'status': hyperopt.STATUS_FAIL}
+            else:
+                return {'status': hyperopt.STATUS_OK, 'loss': rmse}
