7936d33871170b84b2539a24aaac02cd21e69605
dtk <dtk@gmx.de>
2017-06-27 17:11:18 +0200

    Learn Mackey-Glass with MLP from incomplete data

--- examples/mackey_glass/generate_with_manual_feedback.py
+++ examples/mackey_glass/generate_with_manual_feedback.py
@@ -39,6 +39,10 @@ class Example(object):
         self.training_inputs = training_inputs
         self.training_outputs = training_outputs
 
+        # remove many of the training labels to simulate incomplete data
+        self.training_outputs[1::3] = np.nan
+        self.training_outputs[2::3] = np.nan
+
         self.test_inputs = test_inputs
         self.test_outputs = test_outputs
 
@@ -120,7 +124,22 @@ class Example(object):
         self.esn.W_in *= [bias_scale, signal_scale]
 
         # train
-        self.esn.fit(self.training_inputs, self.training_outputs)
+        self.esn.fit(
+            np.array([self.training_inputs[0]]),
+            np.array([self.training_outputs[0]])
+        )
+        for input_date, output_date in zip(
+                self.training_inputs[1:],
+                self.training_outputs[1:]
+        ):
+            if not np.isnan(output_date.item()):
+                self.esn.partial_fit(
+                    np.array([input_date]),
+                    np.array([output_date])
+                )
+            else:
+                # drive reservoir
+                self.esn.predict(input_date)
 
         # test
         predicted_outputs = [self.esn.predict(self.test_inputs[0])]
